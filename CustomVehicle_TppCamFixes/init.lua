9781d67267efd6adf4b0cfe2432dca0cd53c7afd
Si13n7 <admin@si13n7.com>
2025-11-15 18:51:34 +0100

    25.3.22.0

--- CustomVehicle_TppCamFixes/init.lua
+++ CustomVehicle_TppCamFixes/init.lua
@@ -11,7 +11,7 @@ misaligned compared to stock ones.
 ----------------------------------------------
 
 Filename: init.lua
-Version: 2025-03-20, 19:56:27 UTC+01:00 (MEZ)
+Version: 2025-03-21, 11:51 UTC+01:00 (MEZ)
 
 Copyright (c) 2025, Si13n7 Developments(tm)
 All rights reserved.
@@ -22,28 +22,27 @@ ______________________________________________
 --- TweakDB Definition
 ---@class TweakDB
 --- Provides access to game data stored in the database, including camera offsets and various other game settings.
----@field GetFlat fun(self: TweakDB, key: string): Vector3|nil -- Retrieves a value from the database based on the provided key.(self: TweakDB, key: string): Vector3|nil
----@field SetFlat fun(self: TweakDB, key: string, value: Vector3) -- Sets or modifies a value in the database for the specified key.(self: TweakDB, key: string, value: Vector3)
+---@field GetFlat fun(self: TweakDB, key: string): Vector3|nil -- Retrieves a value from the database based on the provided key.
+---@field SetFlat fun(self: TweakDB, key: string, value: Vector3) -- Sets or modifies a value in the database for the specified key.
 TweakDB = TweakDB
 
 --- Game Definition
 ---@class Game
 --- Provides various global game functions, such as getting the player, mounted vehicles, and converting names to strings.
----@field NameToString fun(value: any): string -- Converts a game name object to a readable string.(value: any): string
----@field GetPlayer fun(): Player|nil -- Retrieves the current player instance if available.(): Player|nil
----@field GetMountedVehicle fun(player: Player): Vehicle|nil -- Returns the vehicle the player is currently mounted in, if any.(player: Player): Vehicle|nil
+---@field NameToString fun(value: any): string -- Converts a game name object to a readable string.
+---@field GetPlayer fun(): Player|nil -- Retrieves the current player instance if available.
+---@field GetMountedVehicle fun(player: Player): Vehicle|nil -- Returns the vehicle the player is currently mounted in, if any.
 Game = Game
 
 --- Player Definition
----@class Player
---- Represents the player character in the game, providing functions to interact with the player instance.
----@field SetWarningMessage fun(self: Player, message: string, duration: number): nil -- Displays a warning message on the player's screen for a specified duration.(self: Player, message: string, duration: number): nil
+---@class Player -- Represents the player character in the game, providing functions to interact with the player instance.
+---@field SetWarningMessage fun(self: Player, message: string, duration: number): nil -- Displays a warning message on the player's screen for a specified duration.
 Player = Player
 
 --- Vehicle Definition
 ---@class Vehicle
 --- Represents a vehicle entity within the game, providing functions to interact with it, such as getting the appearance name.
----@field GetCurrentAppearanceName fun(self: Vehicle): string|nil -- Retrieves the current appearance name of the vehicle.(self: Vehicle): string|nil
+---@field GetCurrentAppearanceName fun(self: Vehicle): string|nil -- Retrieves the current appearance name of the vehicle.
 Vehicle = Vehicle
 
 --- Vector3 Definition
@@ -52,69 +51,101 @@ Vehicle = Vehicle
 ---@field x number -- The X-coordinate.
 ---@field y number -- The Y-coordinate.
 ---@field z number -- The Z-coordinate.
----@field new fun(x: number, y: number, z: number): Vector3 -- Creates a new Vector3 instance with specified x, y, and z coordinates.(x: number, y: number, z: number): Vector3
+---@field new fun(x: number, y: number, z: number): Vector3 -- Creates a new Vector3 instance with specified x, y, and z coordinates.
 Vector3 = Vector3
 
 --- Observe Definition
 ---@class Observe
 --- Provides functionality to observe game events, allowing custom functions to be executed when certain events occur.
----@field Observe fun(className: string, functionName: string, callback: fun(...): nil) -- Sets up an observer for a specified function within the game.(className: string, functionName: string, callback: fun(...): nil)
+---@field Observe fun(className: string, functionName: string, callback: fun(...): nil) -- Sets up an observer for a specified function within the game.
 Observe = Observe
 
 --- registerForEvent Definition
 ---@class registerForEvent
 --- Allows the registration of functions to be executed when certain game events occur, such as initialization or shutdown.
----@field registerForEvent fun(eventName: string, callback: fun(...): nil) -- Registers a callback function for a specified event (e.g., 'onInit', 'onShutdown').(eventName: string, callback: fun(...): nil)
+---@field registerForEvent fun(eventName: string, callback: fun(...): nil) -- Registers a callback function for a specified event (e.g., 'onInit', 'onShutdown').
 registerForEvent = registerForEvent
 
 --- spdlog Definition
 ---@class spdlog
 --- Provides logging functionality, allowing messages to be printed to the console or log files for debugging purposes.
----@field error fun(message: string) -- Logs an error message, usually when something goes wrong.(message: string)
+---@field error fun(message: string) -- Logs an error message, usually when something goes wrong.
 spdlog = spdlog
 
 ---@type number
 -- The current debug mode level controlling logging and alerts:
 -- 0 = Disabled; 1 = Alert; 2 = Alert, and Print; 3 = Alert,
 -- Print, and Log
-local devMode = 2
+local devMode = 0
 
 ---@type string
 -- The template string for accessing camera lookAtOffset values in TweakDB.
 local cameraPathTemplate = "Camera.VehicleTPP_%s_%s.lookAtOffset"
 
+---@type string|nil
+-- The currently mounted vehicle name.
+local mountedVehicleName = nil
+
 ---@class VehicleMap
 ---@field [string] string -- Maps vehicle appearance names to their respective camera IDs.
 ---@type VehicleMap|nil
 local defaultVehicleMap = {
-	["archer_hella__basic_player_01"]  = "4w_Archer_Hella",
-	["porsche_911turbo__basic_johnny"] = "4w_911"
+	["archer_hella__basic_player_01"]   = "4w_Archer_Hella",
+	["makigai_maimai__basic_player_01"] = "4w_Makigai",
+	["porsche_911turbo__basic_johnny"]  = "4w_911"
 }
 
 ---@class CameraOffsetPreset
----@field ID string|nil        -- The camera ID used for the vehicle.
----@field Close number|nil     -- The Y-offset for close camera view.
----@field Medium number|nil    -- The Y-offset for medium camera view.
----@field Far number|nil       -- The Y-offset for far camera view.
----@field Link string|nil      -- The name of another vehicle appearance to link to (if applicable).
+---@field ID string|nil -- The camera ID used for the vehicle.
+---@field Close table|nil -- The Y-offset for close camera view.
+---@field Medium table|nil -- The Y-offset for medium camera view.
+---@field Far table|nil -- The Y-offset for far camera view.
+---@field Link string|nil -- The name of another vehicle appearance to link to (if applicable).
 ---@field Shutdown boolean|nil -- Whether to reset to default camera offsets on shutdown.
 ---@type table<string, CameraOffsetPreset>
--- Contains all camera presets and linked vehicles.
+---Contains all camera presets and linked vehicles.
 local cameraOffsetPresets = {
 	-- Fixes
-	["yv_s2000"] = {
-		ID     = "4w_Archer_Hella",
-		Close  = 1.25,
-		Medium = 1.15,
-		Far    = 0.8
-	},
 	["fordgt_base"] = {
 		ID     = "4w_911",
-		Close  = 0.3,
-		Medium = 0.2,
-		Far    = 0.1
+		Close  = { y = 0.3 },
+		Medium = { y = 0.2 },
+		Far    = { y = 0.1 }
 	},
-	-- Links
+	["mini_cooper_s_basic"] = {
+		ID     = "4w_911",
+		Close  = { y = 0.5 },
+		Medium = { y = 0.3 },
+		Far    = { y = 0.1 }
+	},
+	["oranje3_kart"] = {
+		ID     = "4w_Makigai",
+		Close  = { y = 0.7, z = 1.0 },
+		Medium = { y = 0.5, z = 1.2 },
+		Far    = { y = 0.3 }
+	},
+	["toyota_mr2_basic"] = {
+		ID     = "4w_911",
+		Close  = { y = 0.35 },
+		Medium = { y = 0.25 },
+		Far    = { y = 0.15 }
+	},
+	["yv_350z"] = {
+		ID     = "4w_Archer_Hella",
+		Close  = { y = 1.15 },
+		Medium = { y = 1.1 },
+		Far    = { y = 0.9 }
+	},
+	["yv_s2000"] = {
+		ID     = "4w_Archer_Hella",
+		Close  = { y = 1.25 },
+		Medium = { y = 1.15 },
+		Far    = { y = 0.8 }
+	},
+	-- Links instead of duplicates
+	["countachwbk_base"] = {
+		Link = "fordgt_base"
+	},
 	["porsche_911turbo__basic_cabrio_01"] = {
 		Link = "porsche_911turbo__basic_johnny"
 	},
@@ -140,8 +171,9 @@ local cameraOffsetPaths = {
 	"Low_DriverCombatFar"
 }
 
----@param msg string      -- The message to display to the player.
+---@param msg string -- The message to display to the player.
 ---@param secs number|nil -- The duration in seconds for which the message is displayed.
+---Displays a warning message to the player for a specified duration using the 'SetWarningMessage' function.
 local function alert(msg, secs)
 	if not msg then return end
 
@@ -152,7 +184,8 @@ local function alert(msg, secs)
 end
 
 ---@param format string -- The format string for the message.
----@vararg any          -- Additional arguments for formatting the message.
+---@vararg any -- Additional arguments for formatting the message.
+---Logs and displays messages based on the current `devMode` level. Messages can be logged to the console, printed, or shown as alerts.
 local function write(format, ...)
 	if devMode <= 0 then return end
 
@@ -173,6 +206,7 @@ end
 ---@param str string    -- The string to check.
 ---@param prefix string -- The prefix to look for.
 ---@return boolean      -- True if the string starts with the prefix, false otherwise.
+---Checks if a given string begins with the specified prefix.
 local function stringStartsWith(str, prefix)
 	return #prefix <= #str and str:sub(1, #prefix) == prefix
 end
@@ -180,31 +214,36 @@ end
 ---@param id string -- The camera ID.
 ---@param path string -- The camera path to retrieve the offset for.
 ---@return Vector3|nil -- The camera offset as a Vector3.
+---Fetches the current camera offset from 'TweakDB' based on the specified ID and path.
 local function getCameraLookAtOffset(id, path)
 	return TweakDB:GetFlat(string.format(cameraPathTemplate, id, path))
 end
 
----@param id string   -- The camera ID.
+---@param id string -- The camera ID.
 ---@param path string -- The camera path to set the offset for.
----@param x number    -- The X-coordinate of the camera position.
----@param y number    -- The Y-coordinate of the camera position.
----@param z number    -- The Z-coordinate of the camera position.
+---@param x number -- The X-coordinate of the camera position.
+---@param y number -- The Y-coordinate of the camera position.
+---@param z number -- The Z-coordinate of the camera position.
+---Sets a camera offset in 'TweakDB' to the specified position values.
 local function setCameraLookAtOffset(id, path, x, y, z)
 	TweakDB:SetFlat(string.format(cameraPathTemplate, id, path), Vector3.new(x, y, z))
 end
 
----@param id string   -- The camera ID.
+---@param id string -- The camera ID.
 ---@param path string -- The camera path to update.
----@param y number    -- The new Y-coordinate value.
-local function setCameraLookAtOffsetY(id, path, y)
+---@param y number -- The new Y-coordinate value.
+---@param z number|nil -- The optional Z-coordinate value.
+---Updates the Y (and optionally Z) offset of a camera path in 'TweakDB'.
+local function setCameraLookAtOffsetYZ(id, path, y, z)
 	local vec3 = getCameraLookAtOffset(id, path)
 	if vec3 then
-		setCameraLookAtOffset(id, path, vec3.x, y, vec3.z)
+		setCameraLookAtOffset(id, path, vec3.x, y, z or vec3.z)
 	end
 end
 
----@param id string               -- The camera ID.
----@return CameraOffsetPreset|nil -- The camera offset data retrieved from TweakDB.
+---@param id string -- The camera ID.
+---@return CameraOffsetPreset|nil -- The camera offset data retrieved from 'TweakDB'.
+---Retrieves the current camera offset data for the specified camera ID from 'TweakDB' and returns it as a 'CameraOffsetPreset' table.
 local function getCurrentCameraOffset(id)
 	local entry = {
 		ID = id,
@@ -215,12 +254,13 @@ local function getCurrentCameraOffset(id)
 		if not vec3 or not vec3.y then return nil end
 
 		local p = (i - 1) % 3
+		local v = { y = vec3.y, z = vec3.z }
 		if p == 0 then
-			entry.Close = vec3.y
+			entry.Close = v
 		elseif p == 1 then
-			entry.Medium = vec3.y
+			entry.Medium = v
 		else
-			entry.Far = vec3.y
+			entry.Far = v
 		end
 
 		if entry.Far and entry.Medium and entry.Close then
@@ -250,15 +290,26 @@ local function loadDefaultCameraOffsetPresets()
 end
 
 ---@param entry CameraOffsetPreset -- The camera offset data to apply.
--- Applies the specified camera offsets to the vehicle using TweakDB.
-local function applyCameraOffsetPresets(entry)
+-- Applies the specified camera offsets to the vehicle using 'TweakDB'.
+local function applyCameraOffsetPreset(entry)
 	if entry and entry.Link and cameraOffsetPresets[entry.Link] then
 		entry = cameraOffsetPresets[entry.Link]
 	end
 	for i, path in ipairs(cameraOffsetPaths) do
 		local p = (i - 1) % 3
-		local y = p == 0 and entry.Close or p == 1 and entry.Medium or entry.Far
-		setCameraLookAtOffsetY(entry.ID, path, y or 0)
+		local v = p == 0 and entry.Close or p == 1 and entry.Medium or entry.Far
+		if v and v.y then
+			setCameraLookAtOffsetYZ(entry.ID, path, v.y, v.z)
+		end
+	end
+end
+
+-- Restores default camera offsets for vehicles.
+local function applyDefaultCameraOffsetPresets()
+	for _, entry in pairs(cameraOffsetPresets) do
+		if entry.Shutdown then
+			applyCameraOffsetPreset(entry)
+		end
 	end
 end
 
@@ -274,21 +325,26 @@ registerForEvent("onInit", function()
 		if not vehicle then return end
 
 		local name = Game.NameToString(vehicle:GetCurrentAppearanceName())
+		if mountedVehicleName == name then return end
+		mountedVehicleName = name;
+		write("Mounted vehicle: '%s'", name)
+
 		for key, entry in pairs(cameraOffsetPresets) do
 			if name == key or stringStartsWith(name, key) then
-				write("Apply camera preset '%s'.", entry.Link or name)
-				applyCameraOffsetPresets(entry)
+				write("Apply camera preset: '%s'", entry.Link or name)
+				applyCameraOffsetPreset(entry)
 				break
 			end
 		end
+	end)
+
+	Observe("VehicleComponent", "OnUnmountingEvent", function()
+		mountedVehicleName = nil
+		applyDefaultCameraOffsetPresets()
 	end)
 end)
 
 -- Restores default camera offsets for vehicles upon mod shutdown.
 registerForEvent("onShutdown", function()
-	for _, entry in pairs(cameraOffsetPresets) do
-		if entry.Shutdown then
-			applyCameraOffsetPresets(entry)
-		end
-	end
+	applyDefaultCameraOffsetPresets()
 end)
